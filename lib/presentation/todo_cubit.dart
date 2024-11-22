b50001f277c75b24706d061e5ed98bc86ba37649
shroida <shroida012@gmail.com>
2024-11-22 09:41:36 +0200

    todo bubit

--- lib/presentation/todo_cubit.dart
+++ lib/presentation/todo_cubit.dart
@@ -11,5 +11,29 @@ class TodoCubit extends Cubit<List<Todo>> {
 
   Future<void> loadTodos() async {
     final todoList = await todoRepo.getTodos();
+    emit(todoList);
+  }
+
+  Future<void> addTodo(String text) async {
+    final newTodo = Todo(
+        id: DateTime.now().millisecondsSinceEpoch,
+        isCompleted: false,
+        text: text);
+    await todoRepo.addNewTodo(newTodo);
+    loadTodos();
+  }
+
+  Future<void> deleteTodo(Todo todo) async {
+    await todoRepo.deleteTodo(todo);
+    loadTodos();
+  }
+
+  Future<void> updateTodo(Todo todo) async {
+    await todoRepo.updateTodo(todo);
+    loadTodos();
+  }
+
+  Future<void> toggleCompletation(Todo todo) async {
+    todo.toggleCompletion();
   }
 }
