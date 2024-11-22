a43802a0b92a57416138bab2ae0afacca9851c8c
shroida <shroida012@gmail.com>
2024-11-22 10:01:52 +0200

    add show dialog

--- lib/presentation/view/todo_view.dart
+++ lib/presentation/view/todo_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app_bloc/domain/repository/todo_repo.dart';
 import 'package:todo_app_bloc/presentation/todo_cubit.dart';
-import 'package:todo_app_bloc/presentation/view/widgets/TodoViewScreen.dart';
+import 'package:todo_app_bloc/presentation/view/widgets/todo_view_screen.dart';
 
 class TodoView extends StatelessWidget {
   const TodoView({super.key, required this.todoRepo});
